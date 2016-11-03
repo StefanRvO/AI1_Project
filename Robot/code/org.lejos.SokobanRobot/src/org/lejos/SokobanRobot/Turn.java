3031cc09b8679fab2080c9dcbdd56de506afe17a
Theis <rorissian@hotmail.com>
2016-11-04 00:03:25 +0100

    Premerge

--- Robot/code/org.lejos.SokobanRobot/src/org/lejos/SokobanRobot/Turn.java
+++ Robot/code/org.lejos.SokobanRobot/src/org/lejos/SokobanRobot/Turn.java
@@ -71,6 +71,7 @@ public class Turn  extends Thread implements Behavior {
         wait_for_new_crossection();
 
     }
+
     public void wait_for_new_crossection()
     {
         System.out.println("Waiting for CX");
@@ -79,25 +80,38 @@ public class Turn  extends Thread implements Behavior {
         {
             try
             {
-                Thread.sleep(400); //Wait to turn away from line
+                Thread.sleep(150); //Wait to turn away from line
                 RA_R.fill_with_samples(linelight_right.readValue());
                 RA_L.fill_with_samples(linelight_left.readValue());
             }
             catch(InterruptedException e) { }
-            while(Math.abs(RA_R.get_average() - RA_L.get_average()) < 10)
+
+            while(Math.abs(RA_R.get_average() - RA_L.get_average()) < 15)
             {
                 try {
                     Thread.sleep((int)(1./this.frequency) * 1000);
                 }
                 catch(InterruptedException e) { }
+
                 RA_R.add_sample(linelight_right.readValue());
                 RA_L.add_sample(linelight_left.readValue());
 
+                System.out.println( Math.abs( RA_R.get_average() - RA_L.get_average() ) );
+
             }
+
+            cross_detector.set_suspend_crossdector( 500 );
             this.turn_count--;
-            if(this.turn_count > 0) cross_detector.unset_cross_section();
+            if(this.turn_count > 0){
+                cross_detector.unset_cross_section();
+            }
+
         }
+
+
+
     }
+
     public Boolean turning()
     {
         return this.turn_count > 0;
